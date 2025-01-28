24f743ed60696819305f596000a46b5cd0805623
Joao Pedro <joaopo.0607@gmail.com>
2025-01-28 12:28:26 -0300

    exercicio 3 string

--- 1-STRINGS/RESOLUCAO/3.py
+++ 1-STRINGS/RESOLUCAO/3.py
@@ -1,2 +1,8 @@
 # 3. Contar Ocorrências de uma Letra
 # Escreva um programa que peça ao usuário uma palavra e uma letra. O programa deve contar quantas vezes a letra aparece na palavra.
+
+palavra = input('informe uma palavra: ').lower()
+letra = input('informe uma letra: ').lower()
+
+print(palavra.count(letra))
+
