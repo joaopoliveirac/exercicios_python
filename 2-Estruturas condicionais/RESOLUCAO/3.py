b14fcdc875352281eaced6b0079548117fbbab1d
Joao Pedro <joaopo.0607@gmail.com>
2025-01-29 16:31:48 -0300

    exerc 3 estrutura condicional

---
+++ 2-Estruturas condicionais/RESOLUCAO/3.py
@@ -0,0 +1,11 @@
+# 3. Verificar Múltiplo de 5
+# Peça ao usuário um número e diga se ele é múltiplo de 5.
+
+def multiplo_5(n1):
+    if n1 % 5 == 0:
+        print('é multiplo de 5')
+    else:
+        print('não é multiplo de 5')
+
+
+multiplo_5(7)
