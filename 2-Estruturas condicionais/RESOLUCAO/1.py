977b18f2259128b37bf4d4f9756f67a322cb5eda
Joao Pedro <joaopo.0607@gmail.com>
2025-01-29 15:56:30 -0300

    primeiro exercicio de condição

---
+++ 2-Estruturas condicionais/RESOLUCAO/1.py
@@ -0,0 +1,10 @@
+# 1. Par ou Ímpar
+# Peça ao usuário para digitar um número e informe se ele é par ou ímpar.
+
+def par_impar(n1):
+    if n1 % 2 == 0:
+        print('número par')
+    else:
+        print('número impar')
+
+par_impar(2)
