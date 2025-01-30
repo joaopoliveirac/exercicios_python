134c87b48b89249eb1a03abf3836a6936eddb03b
Joao Pedro <joaopo.0607@gmail.com>
2025-01-30 13:39:41 -0300

    exercicio 2 de for

---
+++ 3-Repetições em Python (for)/RESOLUCAO/2.py
@@ -0,0 +1,7 @@
+# 2. Tabuada de um Número
+# Peça ao usuário um número e exiba sua tabuada de 1 a 10.
+
+numero = int(input('informe o numero para saber a tabuada'))
+
+for i in range (1,10+1):
+    print(numero*i)
