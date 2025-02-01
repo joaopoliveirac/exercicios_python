bf75a44ba774f6b0d50a961628601f9b7b564688
Joao Pedro <joaopo.0607@gmail.com>
2025-02-01 16:17:44 -0300

    primeiro exerc de while

---
+++ 4-Repetições em Python (while)/RESOLUCAO/1.py
@@ -0,0 +1,11 @@
+# 1. Contagem de 1 a 10
+# Use um loop while para exibir os números de 1 a 10.
+
+def contador():
+    contagem = 1
+    while contagem <= 10:
+        
+        print(contagem)
+        contagem += 1
+
+contador()
