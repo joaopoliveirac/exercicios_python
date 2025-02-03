636160a78480af7ad68f4249e55a0415dd43ddfd
Joao Pedro <joaopo.0607@gmail.com>
2025-02-03 15:34:35 -0300

    exerc 9 de tuplas

---
+++ 5-TUPLAS/RESOLUCAO/9.py
@@ -0,0 +1,7 @@
+# 9. Maior e Menor Valor em uma Tupla
+# Dada a tupla de números, encontre o maior e o menor valor.
+
+numeros = (15,19,30,1,3,99,54,49)
+maior = max(numeros)
+menor = min(numeros)
+print(f'{maior} \n {menor}')
