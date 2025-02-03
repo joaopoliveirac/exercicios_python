cc383727260c56a4bc903c27af6a9413e9905cd5
Joao Pedro <joaopo.0607@gmail.com>
2025-02-03 11:20:09 -0300

    exerc 4 de tupla

---
+++ 5-TUPLAS/RESOLUCAO/4.py
@@ -0,0 +1,7 @@
+# 4. Contar Elementos em uma Tupla
+# Dada a tupla: numeros = (1, 2, 3, 4, 3, 2, 1, 3, 4, 5)
+# Conte quantas vezes o número 3 aparece.
+
+numeros = (1, 2, 3, 4, 3, 2, 1, 3, 4, 5)
+print(numeros.count(3))
+
