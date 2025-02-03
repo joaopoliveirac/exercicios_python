86876aba03614c5263a23e9fb6dfd01966369c4f
Joao Pedro <joaopo.0607@gmail.com>
2025-02-03 15:28:54 -0300

    exerc 7 de tupla

---
+++ 5-TUPLAS/RESOLUCAO/7.py
@@ -0,0 +1,11 @@
+# 7. Desempacotar uma Tupla
+# Dada a tupla: dados = ("Maria", 25, "Engenheira")
+
+# Desempacote-a em três variáveis e exiba os valores separadamente.
+
+dados = ("Maria", 25, "Engenheira")
+nome = dados[0]
+idade = dados[1]
+profissao = dados[2]
+print(f'{nome} \n {idade} \n {profissao} \n')
+
