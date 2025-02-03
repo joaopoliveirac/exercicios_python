533b09e6c0d2bf00535006347e6e9b746abe14a5
Joao Pedro <joaopo.0607@gmail.com>
2025-02-03 11:01:27 -0300

    exerc 1 tupla

---
+++ 5-TUPLAS/RESOLUCAO/1.py
@@ -0,0 +1,6 @@
+# 1. Criar e Exibir uma Tupla
+# Crie uma tupla com os dias da semana e exiba seu conteúdo.
+
+dias = ('segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo')
+for dia in dias:
+    print(dia)
