fd278d75a99879eba920810d581c0032302df0c0
Joao Pedro <joaopo.0607@gmail.com>
2025-02-03 11:26:30 -0300

    exerc 5 de tupla

---
+++ 5-TUPLAS/RESOLUCAO/5.py
@@ -0,0 +1,5 @@
+# 5. Índice de um Elemento
+# Usando a mesma tupla do exercício anterior, encontre o índice da primeira ocorrência do número 4.
+
+numeros = (1, 2, 3, 4, 3, 2, 1, 3, 4, 5)
+print(numeros.index(4))
