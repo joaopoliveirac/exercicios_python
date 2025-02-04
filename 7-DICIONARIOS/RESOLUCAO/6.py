a78e031889c9ec5f024a13ec4c9906aa55bb8024
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 14:32:18 -0300

    exerc 6 d dicionario

---
+++ 7-DICIONARIOS/RESOLUCAO/6.py
@@ -0,0 +1,6 @@
+# 6. Modificar o Valor de uma Chave
+# Modifique a idade de "Ana" para 24. Exiba o dicionário após a modificação.
+
+dicionario = {'Ana' : 25, 'Joao' : 30, 'Jose': 40}
+dicionario['Ana'] = 37
+print(dicionario)
