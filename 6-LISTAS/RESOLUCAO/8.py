4ffc367ab62436be5233ac892f543f0c1e12347d
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 05:00:00 -0300

    exerc 8 de listas

---
+++ 6-LISTAS/RESOLUCAO/8.py
@@ -0,0 +1,7 @@
+# 8. Concatenar Duas Listas
+# Crie duas listas e una-as em uma terceira lista.
+
+lista_01 = [3,5,9,10,15,35]
+lista_02 = [9,55,77,653]
+lista_03 = lista_01 + lista_02
+print(lista_03)
