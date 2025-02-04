1f6e35aca250c196c37c57b0b7d23b27b05bea33
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 05:49:09 -0300

    desafio 5 de listas

---
+++ 6-LISTAS/RESOLUCAO/desafio_5.py
@@ -0,0 +1,8 @@
+# 5. Converter Lista em String
+# Crie uma lista de palavras e una-as em uma única string separada por espaços.
+
+lista = ['joao', 'pedro', 'oliveira', 'carvalho']
+palavras = ' '.join(lista)
+print(palavras)
+lista = palavras.split()
+print(lista)
