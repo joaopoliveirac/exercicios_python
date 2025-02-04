63e8a987c5c7240188f55507a670a15932300c14
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 04:50:25 -0300

    exerc 4 de lists

---
+++ 6-LISTAS/RESOLUCAO/4.py
@@ -0,0 +1,6 @@
+# 4. Remover Elemento da Lista
+# Remova o nome "Bruno" da lista e exiba a lista após a remoção.
+
+nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+nomes.remove('Bruno')
+print(nomes)
