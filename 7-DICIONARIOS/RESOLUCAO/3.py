1fcd651910d951c19237294dd9c136eda84dce28
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 14:18:39 -0300

    exerc 3 de dicionario

---
+++ 7-DICIONARIOS/RESOLUCAO/3.py
@@ -0,0 +1,6 @@
+# 3. Adicionar um Novo Par Chave-Valor
+# Adicione um novo nome e idade ao dicionário. Exiba o dicionário após a adição.
+
+dicionario = {'joao' : 15, 'pedro': 25, 'jose': 57}
+dicionario['joseudodo'] = 30
+print(dicionario)
