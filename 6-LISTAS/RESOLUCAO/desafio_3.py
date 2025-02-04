71e8cd936753ed4306c919f3f94daf5b721e5126
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 05:43:11 -0300

    desafio 3 de listas

--- 6-LISTAS/RESOLUCAO/desafio_3.py
+++ 6-LISTAS/RESOLUCAO/desafio_3.py
@@ -1,6 +1,9 @@
 # 3. Criar um Sistema de Pontuação
 # Crie um programa que armazene a pontuação de jogadores em uma lista e exiba:
 
+# O maior e menor pontuação
+# A média das pontuações
+
 def armazenar_pontuacao():
     pontuacao_total = []
     while True:
@@ -8,10 +11,15 @@ def armazenar_pontuacao():
         if nome.lower() == 'sair':
             break
         else:
-            pontuacao = input('Informe a pontuacao desse jogador.')
+            pontuacao = int(input('Informe a pontuacao desse jogador.'))
             pontuacao_total.append([nome,pontuacao])
     for nome,pontuacao in pontuacao_total:
         print(f'{nome} tem {pontuacao}')
+    pontuacao = [pontos[1] for pontos in pontuacao_total]
+    maior_pontuacao = max(pontuacao_total, key=lambda x: x[1])
+    menor_pontuacao = min(pontuacao_total, key=lambda x: x[1])
+    media = sum(pontuacao) / len(pontuacao)
+    return maior_pontuacao,menor_pontuacao,media
 
-armazenar_pontuacao()
+print(armazenar_pontuacao())
 
