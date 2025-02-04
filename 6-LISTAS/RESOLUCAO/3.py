aae59995ee6f62433ddf5740b664576d5477b0ef
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 04:46:54 -0300

    exerc 3 de listas

---
+++ 6-LISTAS/RESOLUCAO/3.py
@@ -0,0 +1,6 @@
+# 3. Adicionar Elemento à Lista
+# Adicione o nome "Fernando" à lista de nomes. Depois, exiba a lista atualizada.
+
+nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+nomes.append('Fernando')
+print(nomes)
