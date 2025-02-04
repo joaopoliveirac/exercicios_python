16a6ee0b38d1380d994306b67ea8e5c40168aa3e
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 14:24:06 -0300

    exerc 4 de dicionario

---
+++ 7-DICIONARIOS/RESOLUCAO/4.py
@@ -0,0 +1,6 @@
+# 4. Remover um Par Chave-Valor
+# Dado o dicionário idades, remova o par chave-valor de "Carlos". Exiba o dicionário após a remoção.
+
+dicionario = {'joao' : 15, 'pedro': 25, 'jose': 57, 'carlos' : 55}
+del dicionario['carlos']
+print(dicionario)
