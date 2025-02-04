ebc97e2032661cf5069d608c0a47acf440c49c5d
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 09:14:43 -0300

    exerc 1 dicionario

---
+++ 7-DICIONARIOS/RESOLUCAO/1.py
@@ -0,0 +1,7 @@
+# 1. Criar e Exibir um Dicionário
+# Crie um dicionário com nomes como chaves e idades como valores. Exiba o conteúdo do dicionário.
+
+dicionario = {'joao' : 15, 'pedro': 25, 'jose': 57}
+print(dicionario)
+print(dicionario['joao'])
+
