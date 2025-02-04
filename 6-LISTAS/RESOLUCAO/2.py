84c34ab4ce6434116529468c099caf920528d845
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 04:45:43 -0300

    exerc 2 de listas

---
+++ 6-LISTAS/RESOLUCAO/2.py
@@ -0,0 +1,8 @@
+# 2. Acessar Elementos da Lista
+# Dada a lista: nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+# Peça ao usuário um número de 1 a 5 e exiba o nome correspondente.
+
+nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+numero = int(input('informe um numero de 1 a 5: '))
+print(f'Esse numero equivale ao nome: {nomes[numero]}')
+
