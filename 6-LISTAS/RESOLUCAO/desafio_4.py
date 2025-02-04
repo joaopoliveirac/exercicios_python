679b0af60a26451fdafe33757b308360a01cbdee
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 05:46:34 -0300

    desafio 4 de listas

---
+++ 6-LISTAS/RESOLUCAO/desafio_4.py
@@ -0,0 +1,8 @@
+# 4. Embaralhar uma Lista
+# Crie uma lista de 5 elementos e embaralhe os elementos aleatoriamente. Utilize a função shuffle do módulo random.
+
+import random
+
+lista = ['joao', 'josee', 'theodoro','jose']
+random.shuffle(lista) #altera a lista original.
+print(lista)
