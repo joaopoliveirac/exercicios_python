513e3597095f1af6c59361c3ef61489f9baf8577
Joao Pedro <joaopo.0607@gmail.com>
2025-02-04 05:02:43 -0300

    exerc 9 de listas

---
+++ 6-LISTAS/RESOLUCAO/9.py
@@ -0,0 +1,6 @@
+# 9. Multiplicar Elementos da Lista
+# Crie uma lista de números e multiplique todos os seus elementos por 2.
+
+lista = [2,4,6,8]
+lista_mult = [numero * 2 for numero in lista]
+print(lista_mult)
