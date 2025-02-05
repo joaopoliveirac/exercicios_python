e0076c83289fbd35a6f7276a889737653422cd02
Joao Pedro <joaopo.0607@gmail.com>
2025-02-05 10:11:44 -0300

    exerc 1 e 2 de funcao

---
+++ 8-FUNÇÕES/RESOLUCAO/1.py
@@ -0,0 +1,7 @@
+# 1. Função que Retorna Olá!
+# Crie uma função chamada saudacao() que não recebe argumentos e imprime a mensagem "Olá!" na tela.
+
+def saudacao():
+    return 'ola'
+
+print(saudacao())
---
+++ 8-FUNÇÕES/RESOLUCAO/2.py
@@ -0,0 +1,7 @@
+# 2. Função com Parâmetro de Saída
+# Crie uma função soma(a, b) que recebe dois números e retorna a soma deles.
+
+def soma(a,b):
+    return a + b
+
+print(soma(5,7))
