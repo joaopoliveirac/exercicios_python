e23b95d5e16fe70b3e75ff4cfe27ee33f52e4462
Joao Pedro <joaopo.0607@gmail.com>
2025-02-13 18:11:20 -0300

    iniciando com sqlalchemy

---
+++ 10-SQLALCHEMY/RESOLUCAO/1.py
@@ -0,0 +1,3 @@
+# 1. Criando uma Tabela e Inserindo Dados
+# Crie uma classe Pessoa com os campos id, nome e idade. Crie a tabela no banco de dados e insira alguns registros de pessoas.
+# Objetivo: Criar uma tabela e inserir dados no banco usando SQLAlchemy.
